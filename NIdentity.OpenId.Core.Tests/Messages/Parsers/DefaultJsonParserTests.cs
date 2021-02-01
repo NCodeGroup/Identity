f8d2719d00beca5d857f63693feae6ec574ecad9
Marcin Polewski <marcin@polewski.net>
2021-02-01 14:59:30 -0500

    Adding tests for DefaultJsonParser

---
+++ NIdentity.OpenId.Core.Tests/Messages/Parsers/DefaultJsonParserTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Buffers;
+using System.Text.Json;
+using Moq;
+using NIdentity.OpenId.Messages;
+using NIdentity.OpenId.Messages.Parameters;
+using NIdentity.OpenId.Messages.Parsers;
+using Xunit;
+
+namespace NIdentity.OpenId.Core.Tests.Messages.Parsers
+{
+    public class DefaultJsonParserTests : IDisposable
+    {
+        private readonly MockRepository _mockRepository;
+        private readonly Mock<IOpenIdMessageContext> _mockOpenIdMessageContext;
+
+        public DefaultJsonParserTests()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+            _mockOpenIdMessageContext = _mockRepository.Create<IOpenIdMessageContext>();
+        }
+
+        public void Dispose()
+        {
+            _mockRepository.Verify();
+        }
+
+        [Fact]
+        public void Singleton()
+        {
+            var parser = DefaultJsonParser.Instance;
+
+            Assert.Same(DefaultJsonParser.Instance, parser);
+        }
+
+        [Fact]
+        public void Load_ThenIsValid()
+        {
+            var parser = new DefaultJsonParser();
+
+            const string parameterName = "parameterName";
+
+            var context = _mockOpenIdMessageContext.Object;
+            var jsonSerializerOptions = new JsonSerializerOptions();
+
+            var descriptor = new ParameterDescriptor(parameterName);
+            var parameter = new Parameter(descriptor);
+
+            var buffer = new ArrayBufferWriter<byte>();
+            var writer = new Utf8JsonWriter(buffer);
+
+            var expectedValue = new TestNestedObject { NestedPropertyName1 = "NestedPropertyValue" };
+            var expectedStringValue = JsonSerializer.Serialize(expectedValue);
+            JsonSerializer.Serialize(writer, expectedValue);
+
+            var reader = new Utf8JsonReader(buffer.WrittenSpan);
+
+            parser.Load(context, parameter, ref reader, jsonSerializerOptions);
+
+            Assert.IsType<JsonElement>(parameter.ParsedValue);
+            Assert.Equal(expectedStringValue, parameter.StringValues);
+            Assert.Equal(expectedStringValue, JsonSerializer.Serialize(parameter.ParsedValue));
+        }
+    }
+}

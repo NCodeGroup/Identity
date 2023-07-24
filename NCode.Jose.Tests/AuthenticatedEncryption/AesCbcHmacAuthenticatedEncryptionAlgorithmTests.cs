34871f5a1e2657ad9605c1c1e6425e8e72e93a15
Marcin Polewski <marcin.polewski@gmail.com>
2023-07-24 10:59:36 -0600

    Unit tests for AesCbcHmacAuthenticatedEncryptionAlgorithm

---
+++ NCode.Jose.Tests/AuthenticatedEncryption/AesCbcHmacAuthenticatedEncryptionAlgorithmTests.cs
@@ -0,0 +1,172 @@
+﻿#region Copyright Preamble
+
+//
+//    Copyright @ 2023 NCode Group
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+#endregion
+
+using System.Security.Cryptography;
+using Jose;
+using NCode.Jose.AuthenticatedEncryption;
+
+namespace NCode.Jose.Tests.AuthenticatedEncryption;
+
+public class AesCbcHmacAuthenticatedEncryptionAlgorithmTests
+{
+    private static bool KeyedHashFunctionNotImplemented(
+        ReadOnlySpan<byte> key,
+        ReadOnlySpan<byte> source,
+        Span<byte> destination,
+        out int bytesWritten)
+    {
+        throw new NotImplementedException();
+    }
+
+    [Theory]
+    [InlineData(1, 16)]
+    [InlineData(15, 16)]
+    [InlineData(16, 16)]
+    [InlineData(17, 17 + 16)]
+    [InlineData(32, 32 + 16)]
+    [InlineData(48, 48 + 16)]
+    [InlineData(64, 64 + 16)]
+    public void GetCipherTextSizeBytes_Valid(int plainTextSizeBytes, int expected)
+    {
+        const int cekSizeBits = -1; // don't care
+
+        var algorithm = new AesCbcHmacAuthenticatedEncryptionAlgorithm(
+            "code",
+            KeyedHashFunctionNotImplemented,
+            cekSizeBits);
+
+        var actual = algorithm.GetCipherTextSizeBytes(plainTextSizeBytes);
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(256, 256)]
+    [InlineData(384, 384)]
+    [InlineData(512, 512)]
+    public void GetMaxPlainTextSizeBytes_Valid(int cipherTextSizeBytes, int expected)
+    {
+        const int cekSizeBits = -1; // don't care
+
+        var algorithm = new AesCbcHmacAuthenticatedEncryptionAlgorithm(
+            "code",
+            KeyedHashFunctionNotImplemented,
+            cekSizeBits);
+
+        var actual = algorithm.GetMaxPlainTextSizeBytes(cipherTextSizeBytes);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void CreateAes_Valid()
+    {
+        Span<byte> key = new byte[256 >> 3];
+        RandomNumberGenerator.Fill(key);
+
+        using var aes = AesCbcHmacAuthenticatedEncryptionAlgorithm.CreateAes(key);
+        Assert.True(aes.Key.AsSpan().SequenceEqual(key));
+    }
+
+    public static IEnumerable<object[]> RoundTripTestData
+    {
+        get
+        {
+            yield return new object[]
+            {
+                256,
+                (KeyedHashFunctionDelegate)HMACSHA256.TryHashData,
+                new HmacUsingSha("SHA256")
+            };
+            yield return new object[]
+            {
+                384,
+                (KeyedHashFunctionDelegate)HMACSHA384.TryHashData,
+                new HmacUsingSha("SHA384")
+            };
+            yield return new object[]
+            {
+                512,
+                (KeyedHashFunctionDelegate)HMACSHA512.TryHashData,
+                new HmacUsingSha("SHA512")
+            };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(RoundTripTestData))]
+    public void RoundTrip_Valid(int cekSizeBits, KeyedHashFunctionDelegate keyedHashFunction, IJwsAlgorithm jwsAlgorithm)
+    {
+        const int blockSizeBytes = 128 >> 3;
+        //var plainTextSizeBytes = blockSizeBytes * RandomNumberGenerator.GetInt32(1, 9);
+        const int plainTextSizeBytes = blockSizeBytes - 1;
+
+        var cekSizeBytes = cekSizeBits >> 3;
+        var componentSizeBytes = cekSizeBytes >> 1;
+
+        Span<byte> cek = new byte[cekSizeBytes];
+        Span<byte> plainText = new byte[plainTextSizeBytes];
+        Span<byte> associatedData = new byte[blockSizeBytes];
+
+        RandomNumberGenerator.Fill(cek);
+        RandomNumberGenerator.Fill(plainText);
+        RandomNumberGenerator.Fill(associatedData);
+
+        var controlAlgorithm = new AesCbcHmacEncryption(jwsAlgorithm, cekSizeBits);
+        var expectedResult = controlAlgorithm.Encrypt(associatedData.ToArray(), plainText.ToArray(), cek.ToArray());
+        if (expectedResult is not [var nonce, var expectedCipherText, var expectedAuthTag])
+        {
+            throw new InvalidOperationException();
+        }
+
+        var algorithm = new AesCbcHmacAuthenticatedEncryptionAlgorithm(
+            "code",
+            keyedHashFunction,
+            cekSizeBits);
+
+        var cipherTextSizeBytes = algorithm.GetCipherTextSizeBytes(plainTextSizeBytes);
+
+        Span<byte> cipherText = new byte[cipherTextSizeBytes];
+        Span<byte> authenticationTag = new byte[componentSizeBytes];
+
+        algorithm.Encrypt(
+            cek,
+            nonce.AsSpan(),
+            plainText,
+            associatedData,
+            cipherText,
+            authenticationTag);
+
+        Assert.Equal(expectedCipherText, cipherText.ToArray());
+        Assert.Equal(expectedAuthTag, authenticationTag.ToArray());
+
+        Span<byte> plainTextOutput = new byte[plainTextSizeBytes];
+
+        var decryptResult = algorithm.TryDecrypt(
+            cek,
+            nonce.AsSpan(),
+            cipherText,
+            associatedData,
+            authenticationTag,
+            plainTextOutput,
+            out var decryptBytesWritten);
+
+        Assert.True(decryptResult);
+        Assert.Equal(plainTextSizeBytes, decryptBytesWritten);
+        Assert.Equal(plainText.ToArray(), plainTextOutput.ToArray());
+    }
+}
--- NCode.Jose/AuthenticatedEncryption/AesCbcHmacAuthenticatedEncryptionAlgorithm.cs
+++ NCode.Jose/AuthenticatedEncryption/AesCbcHmacAuthenticatedEncryptionAlgorithm.cs
@@ -1,18 +1,20 @@
 #region Copyright Preamble
-// 
+
+//
 //    Copyright @ 2023 NCode Group
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+
 #endregion
 
 using System.Buffers.Binary;
@@ -67,13 +69,15 @@ public class AesCbcHmacAuthenticatedEncryptionAlgorithm : AuthenticatedEncryptio
 
     /// <inheritdoc />
     public override int GetCipherTextSizeBytes(int plainTextSizeBytes) =>
-        (BlockSizeBytes * plainTextSizeBytes / BlockSizeBytes) + BlockSizeBytes;
+        plainTextSizeBytes > BlockSizeBytes ?
+            BlockSizeBytes * plainTextSizeBytes / BlockSizeBytes + BlockSizeBytes :
+            BlockSizeBytes;
 
     /// <inheritdoc />
     public override int GetMaxPlainTextSizeBytes(int cipherTextSizeBytes) =>
         cipherTextSizeBytes;
 
-    private static Aes CreateAes(ReadOnlySpan<byte> key)
+    internal static Aes CreateAes(ReadOnlySpan<byte> key)
     {
         var aes = Aes.Create();
         Debug.Assert(aes.BlockSize == BlockSizeBits);

2e4fcc6e5fa515a7e2d9c90315d0670dbae88906
Marcin Polewski <marcin.polewski@gmail.com>
2023-07-25 17:36:40 -0600

    Unit tests for DirectKeyManagementAlgorithm

--- NCode.Jose/KeyManagement/DirectKeyManagementAlgorithm.cs
+++ NCode.Jose/KeyManagement/DirectKeyManagementAlgorithm.cs
@@ -1,22 +1,25 @@
 ﻿#region Copyright Preamble
-// 
+
+//
 //    Copyright @ 2023 NCode Group
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+
 #endregion
 
 using System.Security.Cryptography;
 using NCode.Cryptography.Keys;
+using NCode.Jose.Exceptions;
 
 namespace NCode.Jose.KeyManagement;
 
@@ -60,7 +63,7 @@ public class DirectKeyManagementAlgorithm : KeyManagementAlgorithm
 
         if (contentKey.Length != secretKey.KeySizeBytes)
         {
-            throw new ArgumentException("Invalid size for destination buffer.", nameof(contentKey));
+            throw new JoseException("The size of the destination buffer for CEK must identical to the KEK size.");
         }
 
         validatedSecretKey.KeyBytes.CopyTo(contentKey);
@@ -74,7 +77,7 @@ public class DirectKeyManagementAlgorithm : KeyManagementAlgorithm
         Span<byte> encryptedContentKey,
         out int bytesWritten)
     {
-        throw new NotSupportedException("The direct key management algorithm does not support using an existing CEK.");
+        throw new JoseException("The direct key management algorithm does not support using an existing CEK.");
     }
 
     /// <inheritdoc />
@@ -89,9 +92,7 @@ public class DirectKeyManagementAlgorithm : KeyManagementAlgorithm
 
         if (encryptedContentKey.Length != 0)
         {
-            throw new ArgumentException(
-                "The encrypted content encryption key (CEK) does not have a valid size for this cryptographic algorithm.",
-                nameof(encryptedContentKey));
+            throw new JoseException("The encrypted content encryption key (CEK) does not have a valid size for this cryptographic algorithm.");
         }
 
         if (contentKey.Length < secretKey.KeySizeBytes)

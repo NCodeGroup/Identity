82b080646590751af1e7aa33d308e7247adb185c
Marcin Polewski <marcin.polewski@gmail.com>
2023-07-27 17:32:56 -0600

    Unit tests for RsaKeyManagementAlgorithm

--- NCode.Jose.Tests/KeyManagement/RsaKeyManagementAlgorithmTests.cs
+++ NCode.Jose.Tests/KeyManagement/RsaKeyManagementAlgorithmTests.cs
@@ -1,18 +1,20 @@
 ﻿#region Copyright Preamble
-// 
+
+//
 //    Copyright @ 2023 NCode Group
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+
 #endregion
 
 using System.Security.Cryptography;
@@ -24,6 +26,36 @@ namespace NCode.Jose.Tests.KeyManagement;
 
 public class RsaKeyManagementAlgorithmTests : BaseTests
 {
+    [Fact]
+    public void Code_Valid()
+    {
+        const string code = nameof(code);
+
+        var algorithm = new RsaKeyManagementAlgorithm(code, RSAEncryptionPadding.Pkcs1);
+        Assert.Equal(code, algorithm.Code);
+    }
+
+    [Fact]
+    public void KeyType_Valid()
+    {
+        const string code = nameof(code);
+
+        var algorithm = new RsaKeyManagementAlgorithm(code, RSAEncryptionPadding.Pkcs1);
+        Assert.Equal(typeof(RsaSecretKey), algorithm.KeyType);
+    }
+
+    [Fact]
+    public void KeyBitSizes_Valid()
+    {
+        const string code = nameof(code);
+
+        var algorithm = new RsaKeyManagementAlgorithm(code, RSAEncryptionPadding.Pkcs1);
+        var result = Assert.Single(algorithm.KeyBitSizes);
+        Assert.Equal(2048, result.MinSize);
+        Assert.Equal(16384, result.MaxSize);
+        Assert.Equal(64, result.SkipSize);
+    }
+
     public static IEnumerable<object[]> GetCommonRsaKeyParameterTestData()
     {
         yield return new object[] { 2048, RSAEncryptionPadding.Pkcs1 };
@@ -103,22 +135,22 @@ public class RsaKeyManagementAlgorithmTests : BaseTests
         using var secretKey = RsaSecretKey.Create(keyId, key);
 
         var algorithm = new RsaKeyManagementAlgorithm(code, padding);
+        var header = new Dictionary<string, object>();
 
         var cekSizeBytes = algorithm.GetLegalCekByteSizes(kekSizeBits).Single().MaxSize;
-        var keySizeBytes = algorithm.GetEncryptedContentKeySizeBytes(secretKey.KeySizeBits, cekSizeBytes);
+        var encryptedCekSizeBytes = algorithm.GetEncryptedContentKeySizeBytes(secretKey.KeySizeBits, cekSizeBytes);
 
-        var header = new Dictionary<string, object>();
-        var contentKey = new byte[cekSizeBytes];
-        var encryptedContentKey = new byte[keySizeBytes];
+        Span<byte> cek = new byte[cekSizeBytes];
+        Span<byte> encryptedCek = new byte[encryptedCekSizeBytes];
 
-        RandomNumberGenerator.Fill(contentKey);
+        RandomNumberGenerator.Fill(cek);
 
-        var result = algorithm.TryWrapKey(secretKey, header, contentKey, encryptedContentKey, out var bytesWritten);
+        var result = algorithm.TryWrapKey(secretKey, header, cek, encryptedCek, out var bytesWritten);
         Assert.True(result);
-        Assert.Equal(keySizeBytes, bytesWritten);
+        Assert.Equal(encryptedCekSizeBytes, bytesWritten);
 
-        var decrypted = key.Decrypt(encryptedContentKey, padding);
-        Assert.Equal(Convert.ToBase64String(contentKey), Convert.ToBase64String(decrypted));
+        var decrypted = key.Decrypt(encryptedCek.ToArray(), padding);
+        Assert.Equal(Convert.ToBase64String(cek), Convert.ToBase64String(decrypted));
     }
 
     [Fact]
@@ -155,34 +187,53 @@ public class RsaKeyManagementAlgorithmTests : BaseTests
         using var secretKey = RsaSecretKey.Create(keyId, key);
 
         var algorithm = new RsaKeyManagementAlgorithm(code, padding);
+        var header = new Dictionary<string, object>();
 
         var cekSizeBytes = algorithm.GetLegalCekByteSizes(kekSizeBits).Single().MaxSize;
-        var keySizeBytes = algorithm.GetEncryptedContentKeySizeBytes(secretKey.KeySizeBits, cekSizeBytes);
+        var encryptedCekSizeBytes = algorithm.GetEncryptedContentKeySizeBytes(secretKey.KeySizeBits, cekSizeBytes);
 
-        var header = new Dictionary<string, object>();
-        var contentKey = new byte[cekSizeBytes];
-        var encryptedContentKey = new byte[keySizeBytes + 16];
+        Span<byte> cek = new byte[cekSizeBytes];
+        Span<byte> decryptedCek = new byte[cekSizeBytes];
+        Span<byte> encryptedCek = new byte[encryptedCekSizeBytes];
 
-        RandomNumberGenerator.Fill(contentKey);
+        RandomNumberGenerator.Fill(cek);
 
         var wrapResult = algorithm.TryWrapKey(
             secretKey,
             header,
-            contentKey,
-            encryptedContentKey,
-            out var encryptedBytes);
+            cek,
+            encryptedCek,
+            out var wrapBytesWritten);
         Assert.True(wrapResult);
-        Assert.Equal(keySizeBytes, encryptedBytes);
+        Assert.Equal(encryptedCekSizeBytes, wrapBytesWritten);
 
-        var decryptedContentKey = new byte[keySizeBytes + 32];
+        var cekSizeBits = cekSizeBytes << 3;
+        var controlAlgorithm = GetControlAlgorithm(padding);
+        if (controlAlgorithm != null)
+        {
+            var controlResult = controlAlgorithm.Unwrap(encryptedCek.ToArray(), key, cekSizeBits, header);
+            Assert.Equal(cek.ToArray(), controlResult);
+        }
+
         var unwrapResult = algorithm.TryUnwrapKey(
             secretKey,
             header,
-            encryptedContentKey.AsSpan(0, encryptedBytes),
-            decryptedContentKey,
-            out var decryptedBytes);
+            encryptedCek,
+            decryptedCek,
+            out var unwrapBytesWritten);
         Assert.True(unwrapResult);
-        Assert.Equal(cekSizeBytes, decryptedBytes);
-        Assert.Equal(Convert.ToBase64String(contentKey), Convert.ToBase64String(decryptedContentKey.AsSpan(0, decryptedBytes)));
+        Assert.Equal(cekSizeBytes, unwrapBytesWritten);
+        Assert.Equal(cek.ToArray(), decryptedCek.ToArray());
+    }
+
+    private static global::Jose.IKeyManagement? GetControlAlgorithm(RSAEncryptionPadding padding)
+    {
+        if (padding == RSAEncryptionPadding.Pkcs1)
+            return new global::Jose.RsaKeyManagement(false);
+        if (padding == RSAEncryptionPadding.OaepSHA1)
+            return new global::Jose.RsaKeyManagement(true);
+        if (padding == RSAEncryptionPadding.OaepSHA256)
+            return new global::Jose.RsaOaep256KeyManagement();
+        return null;
     }
 }

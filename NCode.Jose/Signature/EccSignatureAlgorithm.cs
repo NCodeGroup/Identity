3e065cb4ea4373c1b3804a36a55274b961c91973
Marcin Polewski <marcin.polewski@gmail.com>
2023-07-27 18:26:22 -0600

    Adding unit tests for EccSignatureAlgorithm

--- NCode.Jose/Signature/EccSignatureAlgorithm.cs
+++ NCode.Jose/Signature/EccSignatureAlgorithm.cs
@@ -1,18 +1,20 @@
 #region Copyright Preamble
-// 
+
+//
 //    Copyright @ 2023 NCode Group
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+
 #endregion
 
 using System.Security.Cryptography;
@@ -46,13 +48,16 @@ public class EccSignatureAlgorithm : SignatureAlgorithm
     /// <param name="hashAlgorithmName">Contains a <see cref="HashAlgorithmName"/> value that specifies the type of hash function to use.</param>
     public EccSignatureAlgorithm(string code, HashAlgorithmName hashAlgorithmName)
     {
-        var hashSize = HashSizeBitsFromAlgorithmName(hashAlgorithmName);
+        var hashSizeBits = HashSizeBitsFromAlgorithmName(hashAlgorithmName);
+        var kekSizeBits = hashSizeBits == 512 ? 521 : hashSizeBits;
+
+        // ECDSA signatures are twice the size of the key size rounded up to the nearest byte
+        var (quotient, remainder) = Math.DivRem(kekSizeBits * 2, 8);
+        SignatureSizeBits = (quotient + remainder) * 8;
 
         Code = code;
-        SignatureSizeBits = hashSize;
         HashAlgorithmName = hashAlgorithmName;
 
-        var kekSizeBits = hashSize == 512 ? 521 : hashSize;
         KeyBitSizes = new[] { new KeySizes(minSize: kekSizeBits, maxSize: kekSizeBits, skipSize: 0) };
     }
 

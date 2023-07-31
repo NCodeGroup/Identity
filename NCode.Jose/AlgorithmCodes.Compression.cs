8b4f9e2bd19d0babe2aa42c7e69c1f7c123d8598
Marcin Polewski <marcin.polewski@gmail.com>
2023-07-31 12:58:16 -0600

    use empty code for none compression algorithm

--- NCode.Jose/AlgorithmCodes.Compression.cs
+++ NCode.Jose/AlgorithmCodes.Compression.cs
@@ -1,18 +1,20 @@
 ﻿#region Copyright Preamble
-// 
+
+//
 //    Copyright @ 2023 NCode Group
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+
 #endregion
 
 namespace NCode.Jose;
@@ -30,7 +32,7 @@ public partial class AlgorithmCodes
         /// <summary>
         /// Specifies that compression is not used.
         /// </summary>
-        public const string None = "none";
+        public const string None = "";
 
         /// <summary>
         /// Compression with the DEFLATE (RFC1951) algorithm.

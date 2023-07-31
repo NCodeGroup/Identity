403e88d917f2d4d35506634d670e02fb7377b041
Marcin Polewski <marcin.polewski@gmail.com>
2023-07-31 13:18:46 -0600

    removing dead code

--- NIdentity.OpenId.Core/Logic/CryptoService.cs
+++ NIdentity.OpenId.Core/Logic/CryptoService.cs
@@ -1,24 +1,26 @@
 #region Copyright Preamble
-// 
+
+//
 //    Copyright @ 2023 NCode Group
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //        http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
+
 #endregion
 
 using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
-using NIdentity.OpenId.Cryptography.Binary;
+using NCode.Encoders;
 
 namespace NIdentity.OpenId.Logic;
 
@@ -37,6 +39,8 @@ public interface ICryptoService
 
 internal class CryptoService : ICryptoService
 {
+    private const int MaxStackAlloc = 512 >> 3;
+
     public void GenerateBytes(Span<byte> destination) =>
         RandomNumberGenerator.Fill(destination);
 
@@ -51,7 +55,7 @@ internal class CryptoService : ICryptoService
 
     public string GenerateKey(int byteLength, BinaryEncodingType binaryEncodingType)
     {
-        var bytes = byteLength <= BinaryUtility.StackAllocMax ?
+        var bytes = byteLength <= MaxStackAlloc ?
             stackalloc byte[byteLength] :
             GC.AllocateUninitializedArray<byte>(byteLength, pinned: false);
 
@@ -71,8 +75,8 @@ internal class CryptoService : ICryptoService
     {
         using var hashAlgorithm = CreateHashAlgorithm(hashAlgorithmType);
 
-        var hashByteLength = hashAlgorithm.HashSize / BinaryUtility.BitsPerByte;
-        var hashBytes = hashByteLength <= BinaryUtility.StackAllocMax ?
+        var hashByteLength = hashAlgorithm.HashSize / 8;
+        var hashBytes = hashByteLength <= MaxStackAlloc ?
             stackalloc byte[hashByteLength] :
             GC.AllocateUninitializedArray<byte>(hashByteLength, pinned: false);
 
@@ -85,7 +89,7 @@ internal class CryptoService : ICryptoService
     public string HashValue(string data, HashAlgorithmType hashAlgorithmType, BinaryEncodingType binaryEncodingType)
     {
         var dataByteLength = Encoding.UTF8.GetByteCount(data);
-        var dataBytes = dataByteLength <= BinaryUtility.StackAllocMax ?
+        var dataBytes = dataByteLength <= MaxStackAlloc ?
             stackalloc byte[dataByteLength] :
             GC.AllocateUninitializedArray<byte>(dataByteLength, pinned: false);
 
